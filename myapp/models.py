bfcb6ab029a6553c5750f0cd71181d5e7fb250eb
sumapriyamcs <93033665+sumapriyamcs@users.noreply.github.com>
2021-12-21 10:06:47 +0530

    Delete myapp directory

--- myapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-# id name description
-#
-class Person(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
